4ba320bd9df91d4a3ba9d5bbd07fdcf8a99e3d6b
Greg Wiley <aztec.rex@jammm.com>
2016-12-31 22:24:39 -0800

    refactor draw

--- gravity_ball.py
+++ gravity_ball.py
@@ -4,16 +4,29 @@ import time
 
 sense = SenseHat()
 
+r = 255
+g = 255
+b = 255
+old_x = 0
+old_y = 0
+def draw(x,y):
+    sense.set_pixel(x,y,r,g,b)
+    if old_x != x or old_y != y:
+        sense.set_pixel(x, y,r, g, b)
+    old_x = x;
+    old_y = y;
+
+
 def clamp(value, min_value=0, max_value=7):
     return min(max_value, max(min_value, value))
 
 def next(v,gv):
-  if gv < 0:
-    return v - 1
-  elif gv > 0:
-    return v + 1
-  else:
-    return v
+    if gv < 0:
+        return v - 1
+    elif gv > 0:
+        return v + 1
+    else:
+        return v
 
 sense.clear()
 x = 3
@@ -22,11 +35,5 @@ while True:
     accel = sense.get_accelerometer_raw()
     next_x = clamp(next(x, accel['x']))
     next_y = clamp(next(y, accel['y']));
-    sense.set_pixel(next_x,next_y,255,255,255)
-    if next_x != x or next_y != y:
-      sense.set_pixel(x,y,0,0,0)
-    x = next_x;
-    y = next_y;
+    draw(next_x, next_y, x, y)
     time.sleep(.15)
-
-
