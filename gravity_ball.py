c59c259eba6458cd8564d32294a6bc3d63fa2a5f
Greg Wiley <aztec.rex@jammm.com>
2016-12-31 21:57:53 -0800

    fix indent

--- gravity_ball.py
+++ gravity_ball.py
@@ -8,12 +8,12 @@ def clamp(value, min_value=0, max_value=7):
     return min(max_value, max(min_value, value))
 
 def next(v,gv):
-    if gv < 0:
-      return v - 1
-    elif gv > 0:
-      return v + 1
+  if gv < 0:
+    return v - 1
+  elif gv > 0:
+    return v + 1
   else:
-      return v
+    return v
 
 x = 3
 y = 3
