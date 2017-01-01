f3ea872eb9edd7401e2ca64b6c31485722926894
Greg Wiley <aztec.rex@jammm.com>
2016-12-31 22:53:14 -0800

    color adjust

--- gravity_ball.py
+++ gravity_ball.py
@@ -17,7 +17,6 @@ def draw(x,y):
     old_x = x;
     old_y = y;
 
-
 def clamp(value, min_value=0, max_value=7):
     return min(max_value, max(min_value, value))
 
@@ -28,6 +27,25 @@ def next(v,gv):
         return v + 1
     else:
         return v
+
+def pushed_up:
+    g = clamp(g + 1, 0, 255)
+
+def pushed_left:
+    r = clamp(r + 1, 0, 255)
+
+def pushed_right:
+    r = clamp(b + 1, 0, 255)
+
+def pushed_down:
+    g = clamp(g - 1, 0, 255)
+    r = clamp(r - 1, 0, 255)
+    b = clamp(b - 1, 0, 255)
+
+sense.stick.direction_up = pushed_up
+sense.stick.direction_down = pushed_down
+sense.stick.direction_left = pushed_left
+sense.stick.direction_right = pushed_right
 
 sense.clear()
 x = 3
